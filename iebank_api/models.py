636e62675d536329e28866d61ca9a7c5ef14f5bb
inds123 <132224864+inds123@users.noreply.github.com>
2024-10-15 12:47:56 -0900

    Update models.py

--- iebank_api/models.py
+++ iebank_api/models.py
@@ -1,22 +1,24 @@
 from iebank_api import db
-from datetime import datetime
-import string, random
 
 class Account(db.Model):
+    __tablename__ = 'accounts'
+
+    # Columns in the accounts table
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False)
-    account_number = db.Column(db.String(20), nullable=False, unique=True)
-    balance = db.Column(db.Float, nullable=False, default = 0.0)
-    currency = db.Column(db.String(1), nullable=False, default="€")
-    status = db.Column(db.String(10), nullable=False, default="Active")
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    name = db.Column(db.String(100), nullable=False)
+    balance = db.Column(db.Float, nullable=False)
+    country = db.Column(db.String(50), nullable=False)  # New field
 
-    def __repr__(self):
-        return '<Event %r>' % self.account_number
-
-    def __init__(self, name, currency):
+    def __init__(self, name, balance, country):
         self.name = name
-        self.account_number = ''.join(random.choices(string.digits, k=20))
-        self.currency = currency
-        self.balance = 0.0
-        self.status = "Active"
+        self.balance = balance
+        self.country = country
+
+    # serialize the account object into JSON format
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'balance': self.balance,
+            'country': self.country
+        }
